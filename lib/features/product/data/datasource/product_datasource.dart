ae363a53807e4eb85b9d37382e54fda97f833ac4
Carlos Vargas <carlos.vr56@gmail.com>
2024-09-18 20:01:45 -0400

    feat: add product_datasource

---
+++ lib/features/product/data/datasource/product_datasource.dart
@@ -0,0 +1,41 @@
+import 'package:async/async.dart';
+import 'package:ecommerce_concept/app_config/http_client/api_client.dart';
+import 'package:ecommerce_concept/features/product/data/models/product_list_response_model.dart';
+
+abstract interface class ProductDatasource {
+  Future<Result<ProductListResponseModel>> fetchProducts({
+    required int limit,
+    required int skip,
+  });
+}
+
+class ProductDatasourceImpl implements ProductDatasource {
+  final APIClient apiClient;
+
+  ProductDatasourceImpl({
+    required this.apiClient,
+  });
+
+  @override
+  Future<Result<ProductListResponseModel>> fetchProducts({
+    required int limit,
+    required int skip,
+  }) async {
+    try {
+      final responseModel = await apiClient.getRequest(
+        '/products',
+        queryParameters: {
+          'limit': limit,
+          'skip': skip,
+        },
+        converter: (json) {
+          return ProductListResponseModel.fromJson(
+              json as Map<String, dynamic>);
+        },
+      );
+      return responseModel;
+    } catch (e) {
+      return Result.error(e);
+    }
+  }
+}
