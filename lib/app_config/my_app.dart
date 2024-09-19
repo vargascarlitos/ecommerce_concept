9ff6362e97a5d64ede6b1324c864fa8724cd875a
Carlos Vargas <carlos.vr56@gmail.com>
2024-09-19 19:21:07 -0400

    feat: add responsive framework

--- lib/app_config/my_app.dart
+++ lib/app_config/my_app.dart
@@ -4,6 +4,7 @@ import 'package:ecommerce_concept/features/product/ui/pages/product_detail_page.
 import 'package:ecommerce_concept/features/product/ui/pages/product_list_page.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:responsive_framework/responsive_framework.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -26,6 +27,31 @@ class MyApp extends StatelessWidget {
               product:
                   ModalRoute.of(context)!.settings.arguments as ProductEntity,
             ),
+      },
+      builder: (context, child) {
+        return ResponsiveBreakpoints.builder(
+          child: Builder(
+            builder: (context) {
+              return ResponsiveScaledBox(
+                width: ResponsiveValue<double?>(
+                  context,
+                  conditionalValues: [
+                    Condition.equals(name: MOBILE, value: 450),
+                    Condition.between(start: 800, end: 1100, value: 800),
+                    Condition.between(start: 1000, end: 1200, value: 700),
+                  ],
+                ).value,
+                child: child!,
+              );
+            },
+          ),
+          breakpoints: [
+            const Breakpoint(start: 0, end: 450, name: MOBILE),
+            const Breakpoint(start: 451, end: 800, name: TABLET),
+            const Breakpoint(start: 801, end: 1920, name: DESKTOP),
+            const Breakpoint(start: 1921, end: double.infinity, name: '4K'),
+          ],
+        );
       },
     );
   }
